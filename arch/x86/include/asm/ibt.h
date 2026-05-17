09e9a1560e3e8099ed85947f3111f0a888f02881
Richard Patel <ripatel@wii.dev>
2026-05-17 13:30:22 -0500

    x86: expose user IBT via PR_CFI_BRANCH_LANDING_PADS
    Allows userspace applications to enable IBT (forward-edge control
    flow integrity protection) using the portable PR_CFI prctl API.
    
    The name 'branch landing pads' is RISC-V specific, but the mechanism
    is nearly identical in x86.
    
    This setting enables the following MSR_IA32_U_CET bits:
    - CET_ENDBR_EN (enforce endbr as indirect branch target)
    - CET_NOTRACK_EN (jump modifier to opt-out of IBT checking)
    
    Kernel-mode IBT (as part of CFI) bans notrack. A future prctl flag
    could be introduced to ban notrack in usermode too.
    
    Signed-off-by: Richard Patel <ripatel@wii.dev>

--- arch/x86/include/asm/ibt.h
+++ arch/x86/include/asm/ibt.h
@@ -116,14 +116,20 @@ static inline void ibt_restore(u64 save) { }
 
 #ifndef __ASSEMBLER__
 
+#include <linux/prctl.h>
+
+#define PR_CFI_SUPPORTED_STATUS_MASK (PR_CFI_ENABLE | PR_CFI_DISABLE | PR_CFI_LOCK)
+
 struct pt_regs;
 
 #ifdef CONFIG_X86_USER_IBT
 bool user_ibt_pop_wait_endbr(struct pt_regs *regs);
 void user_ibt_restore_wait_endbr(struct pt_regs *regs, bool wait_endbr);
+void reset_thread_ibt(void);
 #else
 static inline bool user_ibt_pop_wait_endbr(struct pt_regs *regs) { return false; }
 static inline void user_ibt_restore_wait_endbr(struct pt_regs *regs, bool wait_endbr) {}
+static inline void reset_thread_ibt(void) {}
 #endif /* CONFIG_X86_USER_IBT */
 
 #endif /* __ASSEMBLER__ */
