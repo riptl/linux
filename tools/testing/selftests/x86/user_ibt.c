e50d1685623ee9d68f6698b6fbb96b71094d0e10
Richard Patel <ripatel@wii.dev>
2026-05-24 00:42:15 +0000

    x

--- tools/testing/selftests/x86/user_ibt.c
+++ tools/testing/selftests/x86/user_ibt.c
@@ -10,10 +10,13 @@
 #include <errno.h>
 #include <setjmp.h>
 #include <signal.h>
+#include <stdint.h>
 #include <sys/signal.h>
 #include <sys/syscall.h>
+#include <sys/mman.h>
 #include <linux/const.h>
 #include <linux/prctl.h>
+#include <ucontext.h>
 #include <unistd.h>
 #include "../kselftest.h"
 
@@ -43,7 +46,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 #else
-void __attribute__((naked)) valid_target(void)
+void __attribute__((naked, aligned(4096))) valid_target(void)
 {
 #ifdef __x86_64__
 	asm volatile (
@@ -51,14 +54,14 @@ void __attribute__((naked)) valid_target(void)
 		"ret\n"
 	);
 #else
-        asm volatile (
-                "endbr32\n"
-                "ret\n"
-        );
+	asm volatile (
+		"endbr32\n"
+		"ret\n"
+	);
 #endif
 }
 
-void __attribute__((nocf_check, naked)) invalid_target(void)
+void __attribute__((nocf_check, naked, aligned(4096))) invalid_target(void)
 {
 	asm volatile ("ret\n");
 }
@@ -98,12 +101,12 @@ void __attribute__((naked)) user_ibt_notrack_test(void)
 }
 
 static sigjmp_buf jmpbuf;
-static sig_atomic_t got_signal;
+static sig_atomic_t num_segv;
 static sig_atomic_t got_cperr;
 
 static void segv_handler(int signum, siginfo_t *si, void *uc)
 {
-	got_signal = true;
+	num_segv++;
 	got_cperr = si->si_code == SEGV_CPERR;
 	siglongjmp(jmpbuf, 1);
 }
@@ -111,18 +114,15 @@ static void segv_handler(int signum, siginfo_t *si, void *uc)
 int user_ibt_violation_test(void)
 {
 	struct sigaction sa = {};
-	struct sigaction oldsa;
 	size_t volatile ptr;
 
-	got_signal = false;
+	num_segv = 0;
 	got_cperr = false;
 
 	sa.sa_sigaction = segv_handler;
 	sa.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGSEGV, &sa, &oldsa)) {
-		ksft_perror("SIGSEGV handler setup failed");
-		return 1;
-	}
+	if (sigaction(SIGSEGV, &sa, NULL))
+		return 0;
 
 	if (!sigsetjmp(jmpbuf, 1)) {
 		/*
@@ -136,21 +136,174 @@ int user_ibt_violation_test(void)
 		/* Fall through in case this didn't SIGSEGV */
 	}
 
-	if (sigaction(SIGSEGV, &oldsa, NULL)) {
-		ksft_perror("SIGSEGV handler restore failed");
-		return 1;
+	signal(SIGSEGV, SIG_DFL);
+
+	return num_segv == 1 && got_cperr;
+}
+
+static int user_ibt_signal_handler(void (* handler)(int, siginfo_t *, void *), bool expect_segv)
+{
+	struct sigaction sa = {};
+	pid_t pid = getpid();
+
+	if (pid < 0)
+		return 0;
+	num_segv = 0;
+	got_cperr = false;
+
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGPWR, &sa, NULL))
+		return 0;
+
+	sa.sa_sigaction = segv_handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGSEGV, &sa, NULL))
+		return 0;
+
+	if (kill(getpid(), SIGPWR))
+		return 0;
+
+	signal(SIGSEGV, SIG_DFL);
+	signal(SIGPWR, SIG_DFL);
+
+	return 1;
+}
+
+int user_ibt_signal_handler_valid(void)
+{
+	user_ibt_signal_handler((void *)valid_target, false);
+	return num_segv == 0;
+}
+
+int user_ibt_signal_handler_invalid(void)
+{
+	user_ibt_signal_handler((void *)invalid_target, true);
+	return num_segv == 1 && got_cperr;
+}
+
+static void sigreturn_segv_handler(int signum, siginfo_t *si, void *uc)
+{
+        void * addr = si->si_addr;
+	if (si->si_code != SEGV_ACCERR ||
+            (addr != valid_target && addr != invalid_target))
+		asm volatile("ud2");
+	if (mprotect(addr, 4096, PROT_READ | PROT_EXEC))
+		_exit(1);
+	/* rt_sigreturn to test_page, triggering SIGTRAP */
+}
+
+static sig_atomic_t got_trap;
+
+static void sigreturn_trap_handler(int signum, siginfo_t *si, void *uc)
+{
+	got_trap = 1;
+	siglongjmp(jmpbuf, 1);
+}
+
+int user_ibt_sigreturn(void * target, bool valid)
+{
+	struct sigaction sa = {};
+
+	/*
+	 * Indirect call a non-executable page, triggering a segfault at
+         * the call target. Handle the SIGSEGV by mapping the page
+	 * (containing an int3 instruction). Resume using rt_sigreturn
+	 * to continue at the indirect jump target. Possible outcomes:
+	 *   SIGSEGV with SEGV_CPERR: IBT violation detected
+	 *   SIGTRAP: IBT violation detection broken by signal handler
+	 */
+
+        if (mprotect(target, 4096, PROT_READ))
+                return 0;
+
+	num_segv = false;
+	got_cperr = false;
+
+	sa.sa_sigaction = sigreturn_trap_handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGTRAP, &sa, NULL))
+		return 0;
+
+	sa.sa_sigaction = sigreturn_segv_handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGSEGV, &sa, NULL))
+		return 0;
+
+	if (!sigsetjmp(jmpbuf, 1)) {
+		/* Force an indirect call to test_page */
+		size_t volatile ptr = (size_t)target;
+		((void (* volatile)(void))ptr)();
+		asm volatile("ud2"); /* unreachable */
 	}
 
-	if (!got_signal) {
-		ksft_print_msg("IBT violation did not generate SIGSEGV\n");
-		return 1;
-	}
-	if (!got_cperr) {
-		ksft_print_msg("IBT violation generated unknown segfault\n");
-		return 1;
-	}
+	signal(SIGTRAP, SIG_DFL);
+	signal(SIGSEGV, SIG_DFL);
+        if (mprotect(target, 4096, PROT_READ | PROT_EXEC))
+                return 0;
 
-	return 0;
+	if (valid)
+		return got_trap && !num_segv;
+	else
+		return !got_trap && num_segv && got_cperr;
+}
+
+static sig_atomic_t xsave_ok;
+
+struct fpx_sw_bytes {
+    uint32_t magic1;
+    uint32_t extended_size;
+    uint64_t xfeatures;
+    uint32_t xstate_size;
+    uint32_t padding[7];
+};
+
+struct xsave_hdr {
+    uint64_t xstate_bv;
+};
+
+static void check_xsave_handler(int signum, siginfo_t *si, void *uc_)
+{
+        ucontext_t *uc = uc_;
+        uint8_t * fp = (uint8_t *)uc->uc_mcontext.fpregs;
+        if (!fp)
+                return;
+ struct fpx_sw_bytes *sw =
+        (struct fpx_sw_bytes *)(fp + 464);
+
+        if (sw->magic1 != 0x46505853u)
+                return; /* expect extended XSAVE area when CET enabled */
+
+        if (sw->extended_size < 520)
+                return;
+
+        uint32_t *magic2 = (uint32_t *)(fp + sw->extended_size - sizeof(uint32_t));
+
+        if (*magic2 != 0x46505845u)
+                return;
+
+        uint64_t xstate_bv = *(uint64_t *)(fp + 512);
+
+        xsave_ok = !( xstate_bv & 1UL<<11);
+}
+
+int user_ibt_xsave(void)
+{
+        pid_t pid = getpid();
+        struct sigaction sa = {};
+
+        if (pid < 0)
+                return 0;
+
+        sa.sa_sigaction = check_xsave_handler;
+        sa.sa_flags = SA_SIGINFO;
+        if (sigaction(SIGPWR, &sa, NULL))
+                return 0;
+
+        xsave_ok = false;
+        if (kill(pid, SIGPWR))
+                return 0;
+        return xsave_ok;
 }
 
 int main(int argc, char *argv[])
@@ -172,8 +325,23 @@ int main(int argc, char *argv[])
 	user_ibt_notrack_test();
 	ksft_test_result_pass("notrack indirect call to non-endbr target\n");
 
-	ksft_test_result(!user_ibt_violation_test(),
+	ksft_test_result(user_ibt_violation_test(),
 			 "indirect call to non-endbr target raises SIGSEGV\n");
+
+	ksft_test_result(user_ibt_signal_handler_valid(),
+			 "Signal handler sanity check\n");
+
+	ksft_test_result(user_ibt_signal_handler_invalid(),
+			 "Signal handler without ENDBR raises SIGSEGV\n");
+
+	ksft_test_result(user_ibt_sigreturn(valid_target, true),
+			 "IBT state preserved across signal handler (valid case)\n");
+
+	ksft_test_result(user_ibt_sigreturn(invalid_target, false),
+			 "IBT state preserved across signal handler (missing ENDBR case)\n");
+
+	ksft_test_result(user_ibt_xsave(),
+			 "CET state not visible in sigframe XSAVE\n");
 
 	ksft_finished();
 }
